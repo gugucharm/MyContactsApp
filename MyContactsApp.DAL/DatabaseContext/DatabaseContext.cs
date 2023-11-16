96257b744a32363fde08974d126bc3cf4a96b19d
Maksymilian Mikiewicz <maksmikie2001@gmail.com>
2023-11-16 11:28:14 +0100

    Database context and connection set up

---
+++ MyContactsApp.DAL/DatabaseContext/DatabaseContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyContactsApp.DAL.Models;
+
+namespace MyContactsApp.DAL.DatabaseContext
+{
+    public class MyContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public MyContext(DbContextOptions<MyContext> options) : base(options) { }
+    }
+}
