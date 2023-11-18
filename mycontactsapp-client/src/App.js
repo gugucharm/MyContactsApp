a3481b0940ca7b6c748ae47de2c6c45ba95e75c2
Maksymilian Mikiewicz <maksmikie2001@gmail.com>
2023-11-18 15:22:03 +0100

    Integration with front-end

---
+++ mycontactsapp-client/src/App.js
@@ -0,0 +1,19 @@
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        {/* Add other routes as neeasdasded */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
